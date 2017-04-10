7371b75d39592e4c0e0f923485e02dcae5c11d75
なるみ <comiclo@users.noreply.github.com>
2017-04-10 18:16:22 +0800

    Update improved_iterative_hard_thresholding.m

--- improved_iterative_hard_thresholding.m
+++ improved_iterative_hard_thresholding.m
@@ -1,34 +1,37 @@
 function [optimal_val, optimal_point] = improved_iterative_hard_thresholding(f, df, n , s, alpha_0, sigma, beta, epsilon, max_iter)
 % http://www.personal.soton.ac.uk/hdqi/REPORTS/IIHT.pdf
-% Step 1
+%% Step 0
+% initial point
 x = zeros(n,1);
-update = @(x, alpha) projection(x - alpha * df(x), s);
+
+% x^k(\alpha)
+update = @(x, alpha) projection(x - alpha * df(x), s); 
+
 for k=1:max_iter
-    % Step 2
-    t = 1;
+    %% Step 1
+    t = 1; % \beta^{q_k}
     while f(update(x, alpha_0*t)) > f(x) - (sigma/2)*norm(update(x, alpha_0*t) - x)^2
         t = t * beta;
     end
     x = update(x, alpha_0*t);
-    
-    % Step 3
-     supp = x~=0;
-     grad = df(x);
-     if norm(grad - supp .* grad) <= epsilon
-         break;
-     end
+    fprintf('#%d: optimal value: %f, optimal point: %s\n', k, f(x), mat2str(x));
+
+    %% Step 2
+    if norm(df(x) .* (x~=0)) <= epsilon
+        break;
+    end
 end
 optimal_val = f(x);
 optimal_point = x;
 end
 
-function x_proj = projection(x, s)
+function y = projection(x, s)
 % P_{R_+}
-x = x - (x<0) .* x;
+x = x .* (x>0);
 % P_{S_+}
 [~ , I] = sort(x, 'descend');
-x_proj = zeros(size(x));
+y = zeros(size(x));
 for i=1:s
-    x_proj(I(i)) = x(I(i));
+    y(I(i)) = x(I(i));
 end
 end
